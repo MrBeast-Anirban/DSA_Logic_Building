dd65c586b77319f94f6bb9a21e90298bf2b7488f
Anirban Maitra <maitraanirban25@gmail.com>
2026-03-24 11:58:00 +0530

    First problem

---
+++ problems/1.cpp
@@ -0,0 +1,15 @@
+// PROBLEM: Take a number and print whether it’s positive, negative, or zero.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    if (n > 0) cout << "Positive";
+    else if (n < 0) cout << "Negative";
+    else cout << "Zero";
+
+    return 0;
+}
