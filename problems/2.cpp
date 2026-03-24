407b5dfa42efa24f91a1efbb7d0a19bcf659f044
Anirban Maitra <maitraanirban25@gmail.com>
2026-03-24 12:52:01 +0530

    2.cpp

---
+++ problems/2.cpp
@@ -0,0 +1,11 @@
+// PROBLEM: Check if a number is even or odd.
+
+#include<iostream>
+using namespace std;
+
+int main(){
+	int n;
+	cin >> n;
+	if(n % 2 == 0) cout << "Even";
+	else cout << "Odd";
+}	
