dd58dd5884e20b4fb6aaf4b1aad37e07b9928ced
Anirban Maitra <maitraanirban25@gmail.com>
2026-04-02 21:23:58 +0530

    am and pm program modified

--- problems/AMorPM.cpp
+++ problems/AMorPM.cpp
@@ -6,12 +6,10 @@ int main() {
     int hours, minutes;
     cout << "Enter the time in 24-hour format (hours and minutes): ";
     cin >> hours >> minutes;
-
     if (hours < 12) {
         cout << "The time is: " << hours << ":" << minutes << " AM" << endl;
     } else {
         cout << "The time is: " << hours - 12 << ":" << minutes << " PM" << endl;
     }
-
     return 0;
 }
