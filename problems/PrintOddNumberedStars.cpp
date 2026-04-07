11a31b23bbf51bbbc895d01308af92ac95738fdd
Anirban Maitra <maitraanirban25@gmail.com>
2026-04-07 15:03:02 +0530

    one problem

---
+++ problems/PrintOddNumberedStars.cpp
@@ -0,0 +1,15 @@
+// PROBLEM: Print Stars in Odd Numbers (1, 3, 5, 7, 9)
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout << "Enter the number of rows to print: ";
+    cin >> n;
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= 2*i-1; j++){
+            cout << "*";
+        }
+        cout << endl;
+    }
+    return 0;
+}
