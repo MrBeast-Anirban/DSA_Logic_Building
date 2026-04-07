cf1568defeefa53c59e81e5a3cb898bc14abca36
Anirban Maitra <maitraanirban25@gmail.com>
2026-04-07 16:24:36 +0530

    one problem

---
+++ problems/CounterPattern.cpp
@@ -0,0 +1,13 @@
+// PROBLEM: Print Increaing Numbers per Row (Same Number Repeated) starting from k.
+#include<iostream>
+using namespace std;
+int main(){
+    int n, k;
+    cin >> n >> k;
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= i; j++){
+            cout << k++ << " ";
+        }
+        cout << endl;
+    }
+}
