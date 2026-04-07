4a2b9b66a1a2f4c82231437db10fbe1e601622a9
Anirban Maitra <maitraanirban25@gmail.com>
2026-04-07 15:06:59 +0530

    one problem

---
+++ problems/StarPyramid.cpp
@@ -0,0 +1,18 @@
+// PROBLEM: Print a Centered Pyramid of Stars
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout << "Enter the number of rows to print: ";
+    cin >> n;
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= n-i; j++){
+            cout << " ";
+        }
+        for(int k = 1; k <= 2*i-1; k++){
+            cout << "*";
+        }
+        cout << endl;
+    }
+    return 0;   
+}
