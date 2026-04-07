d932dfbd2f39bc0d97b46488f1a94fea4cf8399d
Anirban Maitra <maitraanirban25@gmail.com>
2026-04-07 16:07:06 +0530

    one problem

---
+++ problems/AlteringStarAndSpace.cpp
@@ -0,0 +1,21 @@
+// PROBLEM: Print Stars and Spaces Alternating (Stars and Blank Spaces)
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout << "Enter the number of rows to print: ";
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n-i-1; j++){
+            cout << "b";
+        }
+        for(int j = 0; j <= 2*i+1; j++){
+            if(j%2 == 0){
+                cout << "b";
+            }
+            else cout << "*";
+        }
+        cout << endl;
+    }
+    return 0;
+}
