b54fe4a7869c71905214d0b2035a5f39f8ae485e
Anirban Maitra <maitraanirban25@gmail.com>
2026-04-07 16:27:22 +0530

    one problem

--- problems/ModCountingPattern.cpp
+++ problems/ModCountingPattern.cpp
@@ -1,4 +1,4 @@
-// PROBLEM: Print Increasing Numbers per Row (Same Number Repeated) starting from k.
+// PROBLEM: Print Increasing Numbers per Row (Same Number Repeated) starting from k and (0<=k<=9).
 #include<iostream>
 using namespace std;
 int main(){
@@ -6,7 +6,7 @@ int main(){
     cin >> n >> k;
     for(int i = 1; i <= n; i++){
         for(int j = 1; j <= i; j++){
-            cout << k++ << " ";
+            cout << k++%10 << " ";
         }
         cout << endl;
     }
