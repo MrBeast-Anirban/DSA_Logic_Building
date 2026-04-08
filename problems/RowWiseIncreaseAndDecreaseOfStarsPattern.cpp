4fd4ff59084c4560240acdf65c80bff0d3ccaa9f
Anirban Maitra <maitraanirban25@gmail.com>
2026-04-08 22:31:36 +0530

    one problem

---
+++ problems/RowWiseIncreaseAndDecreaseOfStarsPattern.cpp
@@ -0,0 +1,29 @@
+// PROBLEM: Write a program to print the increasing and decreasing pattern of stars in each row. The number of rows is given as input.
+/* 
+start
+*
+**
+***
+**
+*
+end
+*/
+#include<iostream>
+using namespace std;
+int main()[
+    int n;
+    cin >> n;
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= i; j++){
+            cout << "*";
+        }
+        cout << endl;
+    }
+    for(int i = n-1; i >= 1; i--){
+        for(int j = 1; j <= i; j++){
+            cout << "*";
+        }
+        cout << endl;
+    }
+    return 0;
+]
