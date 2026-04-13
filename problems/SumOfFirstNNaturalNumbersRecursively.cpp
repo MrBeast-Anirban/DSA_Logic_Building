321a9332c6475eac46d27322d03bde9b81b0ea91
Anirban Maitra <maitraanirban25@gmail.com>
2026-04-13 17:29:48 +0530

    one problem

---
+++ problems/SumOfFirstNNaturalNumbersRecursively.cpp
@@ -0,0 +1,13 @@
+// PEOBLEM: Print sum of first n natural numbers recursively.
+#include<iostream>
+using namespace std;
+int findSum(int n){
+    if(n == 0) return 0; // Base case: sum of first 0 natural numbers is 0
+    return n + findSum(n - 1); // Recursive case: n + sum of first (n-1) natural numbers
+}
+int main(){
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    cout << "The sum of the first " << n << " natural numbers is: " << findSum(n) << endl;
+}
