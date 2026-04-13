03d1f817cba12e20f439e47a343ea9b2a82a80da
Anirban Maitra <maitraanirban25@gmail.com>
2026-04-13 14:16:31 +0530

    one problem

---
+++ problems/PrintFrom1toNUsingRecursion.cpp
@@ -0,0 +1,18 @@
+// PROBLEM: Print from 1 to N using recursion.
+#include<iostream>
+using namespace std;
+
+void printNumbers(int n) {
+    if(n == 0) return;
+    printNumbers(n - 1);
+    cout << n << " ";
+}
+
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    cout << "Numbers from 1 to " << n << " are: ";
+    printNumbers(n);
+    return 0;
+}
