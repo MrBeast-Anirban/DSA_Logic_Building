dabc01fb725d465406e8bbcf36f99d14cfdc1417
Anirban Maitra <maitraanirban25@gmail.com>
2026-04-14 11:49:23 +0530

    one problem

---
+++ problems/LineOfNStarsRecursively.cpp
@@ -0,0 +1,18 @@
+// PROBLEM: Print a line of n stars recursively.
+#include<iostream>
+using namespace std;
+
+void printStars(int n) {
+    if (n == 0) return;
+    cout << "*";
+    printStars(n - 1);
+}
+
+int main() {
+    int n;
+    cout << "Enter the number of stars: ";
+    cin >> n;
+    printStars(n);
+    cout << endl;
+    return 0;
+}
