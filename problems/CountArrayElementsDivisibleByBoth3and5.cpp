1d2d599f135401eea3f3db4851657f405fbd890f
Anirban Maitra <maitraanirban25@gmail.com>
2026-04-22 09:59:25 +0530

    one problem

---
+++ problems/CountArrayElementsDivisibleByBoth3and5.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n, count = 0;
+    cout << "Enter the size of the array: ";
+    cin >> n;
+    if(n <= 1){
+        cout << "Invalid size of the array" << endl;
+        return 0;
+    }
+    int arr[n];
+    cout << "Enter the elements of the array: ";
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+        if(arr[i] %3 == 0 && arr[i]%5 == 0) count++;
+    }
+    cout << "The number of elements divisible by both 3 and 5 is: " << count << endl;
+    return 0;
+}
