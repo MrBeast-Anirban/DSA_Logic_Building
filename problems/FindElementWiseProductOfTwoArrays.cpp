286919faf87f378b3db4f64b3a3e4f7391a8cfaf
Anirban Maitra <maitraanirban25@gmail.com>
2026-04-23 19:59:46 +0530

    one problem

---
+++ problems/FindElementWiseProductOfTwoArrays.cpp
@@ -0,0 +1,41 @@
+// PROBLEM: Find element-wise product of two arrays (A[i] * B[i]).
+#include<iostream>
+using namespace std;
+int main(){
+    int n1,n2;
+    cout << "Enter the size of the first array: ";
+    cin >> n1;
+    if(n1 <= 0){
+        cout << "Invalid size error." << endl;
+        return 0;
+    }
+    cout << "Enter the size of the second array: ";
+    cin >> n2;
+    if(n2 <= 0){
+        cout << "Invalid size error." << endl;
+        return 0;
+    }
+    int arr1[n1], arr2[n2];
+    cout << "Enter the elements of the first array: ";
+    for(int i = 0; i < n1; i++){
+        cin >> arr1[i];
+    }
+    cout << "Enter the elements of the second array: ";
+    for(int i = 0; i < n2; i++){
+        cin >> arr2[i];
+    }
+    if(n1 != n2){
+        cout << "Error: Arrays must be of the same size to find element-wise product." << endl;
+        return 0;
+    }
+    int ProductArray[n1];
+    for(int i = 0; i < n1; i++){
+        ProductArray[i] = arr1[i] * arr2[i];
+    }
+    cout << "The element-wise product of the two arrays is: ";
+    for(int i = 0; i < n1; i++){
+        cout << ProductArray[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
