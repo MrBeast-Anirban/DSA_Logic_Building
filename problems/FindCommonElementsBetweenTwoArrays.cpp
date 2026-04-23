ad4d39b9467189ee15aae9500fca6012683d0fe9
Anirban Maitra <maitraanirban25@gmail.com>
2026-04-23 19:51:48 +0530

    one problem

---
+++ problems/FindCommonElementsBetweenTwoArrays.cpp
@@ -0,0 +1,38 @@
+// PROBLEM: Find the common elements between two arrays.
+#include<iostream>
+using namespace std;
+int main(){
+    int n1,n2;
+    cout << "Enter the size of the first array: ";
+    cin >> n1;
+    if(n1 <= 0){
+        cout << "Invalid size error." << endl;
+        return 0;
+    }
+    cout << "Enter the size of the second array: ";
+    cin >> n2;
+    if(n2 <= 0){
+        cout << "Invalid size error." << endl;
+        return 0;
+    }
+    int arr1[n1], arr2[n2];
+    cout << "Enter the elements of the first array: ";
+    for(int i = 0; i < n1; i++){
+        cin >> arr1[i];
+    }
+    cout << "Enter the elements of the second array: ";
+    for(int i = 0; i < n2; i++){
+        cin >> arr2[i];
+    }
+    cout << "The common elements are: ";
+    for(int i = 0; i < n1; i++){
+        for(int j = 0; j < n2; j++){
+            if(arr1[i] == arr2[j]){
+                cout << arr1[i] << " ";
+                break;
+            }
+        }
+    }
+    cout << endl;
+    return 0;
+}
