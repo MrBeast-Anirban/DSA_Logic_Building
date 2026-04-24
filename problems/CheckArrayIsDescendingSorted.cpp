4ed2cad1c6ad0baf962e5cc28431f46907739ad5
Anirban Maitra <maitraanirban25@gmail.com>
2026-04-24 09:57:14 +0530

    one problem

---
+++ problems/CheckArrayIsDescendingSorted.cpp
@@ -0,0 +1,36 @@
+// PROBLEM: Check if the array is sorted in descending order.
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+    int n;
+    cout << "Enter the size of the array: ";
+    cin >> n;
+    if(n <= 0){
+        cout << "Invalid size error." << endl;
+        return 0;
+    }
+
+    vector<int> arr(n);
+    cout << "Enter the elements of the array: ";
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+
+    bool isSorted = true;
+    for(int i = 0; i < n - 1; i++){
+        if(arr[i] < arr[i + 1]){
+            isSorted = false;
+            break;
+        }
+    }
+
+    if(isSorted){
+        cout << "The array is sorted in descending order." << endl;
+    } else {
+        cout << "The array is not sorted in descending order." << endl;
+    }
+
+    return 0;
+}
