6ac4d45628ab701ea4a2da9869d1c6e11102eab2
Anirban Maitra <maitraanirban25@gmail.com>
2026-04-27 12:07:23 +0530

    one problem

---
+++ problems/CountWordsInASentence.cpp
@@ -0,0 +1,22 @@
+// PROBLEM: Count how many words are in a sentence.
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    string s;
+    cout << "Enter a sentence: ";
+    getline(cin, s);
+    int count = 0;
+    for(int i = 0; i < s.length(); i++){
+        if(s[i] == ' '){
+            count++;
+        }
+    }
+    cout << "Number of words: " << count + 1 << endl;
+    return 0;
+}
+/*
+Enter a sentence: anirban maitra
+Number of words: 2
+*/
