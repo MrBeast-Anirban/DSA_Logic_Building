c36e77cf9fe90ec425fac7eb5a72f2a235136c20
Anirban Maitra <maitraanirban25@gmail.com>
2026-05-12 11:32:01 +0530

    one problem

--- problems/ReverseWordInAStringIfThereLengthIsEven_Way2.cpp
+++ problems/ReverseWordInAStringIfThereLengthIsEven_Way2.cpp
@@ -1,5 +1,4 @@
 // PROBLEM: Reverse the word of a sentence if the length of the word is even. (Another Approach)
-
 #include<iostream>
 #include<string>
 #include<sstream>
@@ -7,6 +6,7 @@ using namespace std;
 
 int main(){
     string sentence;
+    cout << "Enter the sentence: ";
     getline(cin, sentence);
     
     stringstream ss(sentence);
@@ -21,3 +21,7 @@ int main(){
     cout << endl;
     return 0;
 }
+/*
+Enter the sentence: It doesnot matter who I am underneath, but what I do that defines me.
+tI doesnot rettam who I ma underneath, but tahw I od taht defines me.
+*/
